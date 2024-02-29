4a6277d9c442a032e928f5e8a92987da89bb45ce
rkeerthient <rkeerthi@yext.com>
2024-02-29 16:15:06 -0500

    added CTAs

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -1,19 +1,25 @@
 import { CardProps } from "@yext/search-ui-react";
-import { BsClock, BsGlobe, BsPin } from "react-icons/bs";
-import { CiPhone } from "react-icons/ci";
-import { LiaDirectionsSolid } from "react-icons/lia";
-import HoursText from "./HoursText";
-import { useMapContext } from "./Locator";
-import Ce_allEntities from "../types/all_entities";
-import { PiBank } from "react-icons/pi";
+import { BsPin } from "react-icons/bs";
+import { CiCalendar, CiPhone } from "react-icons/ci";
 import { FaArrowRight } from "react-icons/fa6";
+import { FaDirections } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+import Ce_allEntities from "../types/all_entities";
 
 const LocationCard = ({ result }: CardProps<Ce_allEntities>) => {
   // const { hoveredLocationId, setHoveredLocationId, setClicked } =
   //   useMapContext();
 
   const { name } = result;
-  const { id, address, mainPhone, c_category, photoGallery } = result.rawData;
+  const {
+    id,
+    address,
+    mainPhone,
+    c_category,
+    photoGallery,
+    c_primaryCTA,
+    c_secondaryCTA,
+  } = result.rawData;
   console.log(JSON.stringify(result.rawData));
 
   const getDirectionsUrl = (addr?: any) => {
@@ -24,12 +30,9 @@ const LocationCard = ({ result }: CardProps<Ce_allEntities>) => {
     return url;
   };
 
-  //Investment services - https://www.nicepng.com/png/detail/416-4162419_my-investment-comments-value-added-services-icon.png
-  //Fin consultant - https://cdn-icons-png.flaticon.com/512/6684/6684013.png
-
   return (
     <div
-      className="border rounded-sm bg-[#f9f7f6] p-4 flex gap-2 hover:border-[#d61f2b] items-center justify-between"
+      className="border rounded-sm bg-[#f9f7f6] py-4 pl-4 flex gap-2 hover:border-[#d61f2b] items-center justify-between"
       // onMouseEnter={() => {
       //   setHoveredLocationId(id);
       // }}
@@ -91,87 +94,33 @@ const LocationCard = ({ result }: CardProps<Ce_allEntities>) => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-2 hover:underline hover:cursor-pointer items-center text-sm text-[#8982a7]">
-          <FaArrowRight /> <div>contact</div>
-        </div>
-        <div className="flex gap-2 hover:underline hover:cursor-pointer items-center text-sm text-[#8982a7]">
-          <FaArrowRight /> <div>contact</div>
-        </div>
+      <div className="flex flex-col gap-4 w-1/3">
+        {c_primaryCTA && (
+          <a
+            href={c_primaryCTA.link || `#`}
+            className="flex gap-2 hover:underline hover:cursor-pointer items-center text-sm text-[#8982a7]"
+          >
+            {c_primaryCTA.label == "Get Directions" ? (
+              <FaDirections />
+            ) : (
+              <CiCalendar />
+            )}
+            <div>{c_primaryCTA.label}</div>
+          </a>
+        )}
+        {c_secondaryCTA && (
+          <div className="flex gap-2 hover:underline hover:cursor-pointer items-center text-sm text-[#8982a7]">
+            {["Visit my page", "Visit our site"].includes(
+              c_secondaryCTA.label!
+            ) ? (
+              <TbWorldWww />
+            ) : (
+              <FaArrowRight />
+            )}{" "}
+            <div>{c_secondaryCTA.label}</div>
+          </div>
+        )}
       </div>
-      {/* <div className="flex flex-col ">
-        <div className="flex w-full">
-          <div className="flex flex-col justify-between gap-4 ">
-            <a
-              href={landingPageUrl}
-              className="text-lg text-[#348daf] flex gap-4 items-center hover:underline"
-            >
-              {name}
-            </a>
-            <div className="flex items-center gap-2">
-              <div>
-                <BsClock />
-              </div>
-              <div>
-                {hours ? (
-                  <HoursText timezone={timezone} hours={hours} />
-                ) : (
-                  <>Fill in your hours</>
-                )}
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <div>
-                <CiPhone />
-              </div>
-              <div>
-                {mainPhone &&
-                  mainPhone
-                    .replace("+1", "")
-                    .replace(/\D+/g, "")
-                    .replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3")}
-              </div>
-            </div>
-            <div className="flex items-base gap-2">
-              <div>
-                <BsPin className="mt-2" />
-              </div>
-              <div className="flex flex-col text-gray-600 text-sm">
-                <div>{address?.line1}</div>
-                <div>
-                  {address?.city}, {address?.region} {address?.postalCode}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex mt-2">
-          <div className="flex flex-col justify-between gap-4  ">
-            <div>{description}</div>
-            <div className="flex gap-4  items-center justify-between w-full">
-              <div className="m-auto flex flex-col gap-6">
-                <a
-                  target="_blank"
-                  href={`${getDirectionsUrl(address)}`}
-                  className="w-52 uppercase bg-[#027da5] flex justify-center items-center gap-2  text-sm text-white hover:text-white border-2 border-[#027da5] hover:bg-[#027da5] hover:cursor-pointer font-bold text-center rounded-sm px-4 py-2"
-                >
-                  <LiaDirectionsSolid className="w-4 h-4" />
-                  Get Directions
-                </a>
-              </div>
-              <div className="m-auto flex flex-col gap-6">
-                <a
-                  href={`/${slug}`}
-                  className="w-52 mx-auto uppercase bg-[#027da5] flex justify-center items-center gap-2  text-sm text-white hover:text-white border-2 border-[#027da5] hover:bg-[#027da5] hover:cursor-pointer font-bold text-center rounded-sm px-4 py-2"
-                >
-                  <BsGlobe className="w-4 h-4" />
-                  Visit page
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
